91f3ad041cc26d2936c0d303386835064842dd2a
KongLinGui <1751188484@qq.com>
2019-10-26 13:20:05 +0800

    Playerprefs

---
+++ PlayerprefsTest/Assets/Scripts/ZhuJieMianGuanLi.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZhuJieMianGuanLi : MonoBehaviour {
+    public GameObject[] objects;
+
+	void Start () {
+        int num = PlayerPrefs.GetInt("PlayerprefsChuCun");
+        if (num!=0)
+        {
+            objects[0].SetActive(false);
+            objects[num].SetActive(true);
+        }
+        else
+        {
+            objects[num].SetActive(true);
+        }      
+	}	
+}
---
+++ PlayerprefsTest/Assets/Scripts/_Playerprefs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _Playerprefs : MonoBehaviour {
+
+    public void SetShuJu(int num)
+    {
+        SceneManager.LoadScene(num);
+        PlayerPrefs.SetInt("PlayerprefsChuCun", num);
+    }
+
+    public void FanHui()
+    {
+        PlayerPrefs.SetInt("PlayerprefsChuCun", 0);
+    }
+
+    public void GetShuJu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
